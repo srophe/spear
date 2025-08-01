b85676cfc389944f5fe9664a512a13b014cfa905
Erin Geier <115035002+emgeier@users.noreply.github.com>
2025-08-01 07:52:47 -0500

    Update search.js

--- search.js
+++ search.js
@@ -27,7 +27,11 @@ export async function fetchFactoidsWithFilters(state) {
       description: binding.description?.value || '',
       person: binding.person?.value || '',
       label: binding.label?.value || '',
-      gender: binding.gender?.value?.split('/').pop() || ''
+      gender: binding.gender?.value?.split('/').pop() || '',
+      uncertainty: binding.level?.value || '',
+      type: binding.type?.value || '',
+      relationship: binding.relationship?.value || '',
+      event: binding.event?.value || '' 
     }));
   } catch (error) {
     console.error('Failed to fetch factoids:', error);
@@ -115,7 +119,7 @@ PREFIX sp: <http://syriaca.org/prop/>
 PREFIX sps: <http://syriaca.org/prop/statement/>
 PREFIX spq: <http://syriaca.org/prop/qualifier/>
 
-SELECT DISTINCT ?factoid ?description ?person ?label ?gender
+SELECT DISTINCT ?factoid ?description ?person ?label ?gender ?type 
 FROM <http://syriaca.org/persons#graph>
 FROM <https://spear-prosop.org>
 WHERE {
@@ -130,123 +134,11 @@ WHERE {
   }
 }
 ORDER BY ?label
-LIMIT 200
+LIMIT 20
 `;
 }
 
-// BUILD THE SQL QUERIES CUSTOM, MULTIFIELD Union, but ethnicity broken
-// export function buildFilterQuery(state) {
-  
-//   const eventPatterns = Array.from(state.selectedEventKeywords || []).map(uri => `
-//     {
-//       ?statementNode sps:event-keyword <${uri}> .
-//     }
-//   `);
 
-//   const relationshipPatterns = Array.from(state.selectedRelationshipKeywords || []).map(uri => `
-//     {
-//       ?person <${uri}> ?statementNode .
-//     }
-//   `);
-
-//   const ethnicityPatterns = Array.from(state.selectedEthnicityKeywords || []).map(uri => `
-//     {
-//         ?person swdt:ethnic-label <${uri}> .
-//         ?person sp:ethnic-label ?statementNode .
-//     }
-//     `);
-
-//   const ethnicityBlock = ethnicityPatterns.length
-//     ? `FILTER EXISTS {
-//         ${ethnicityPatterns.join('\n      UNION\n')}
-//         }`
-//     : '';
-
-//   const genderPattern = Array.from(state.selectedGenderKeywords || []).map(uri => `
-//     {
-//       ?person swdt:gender <${uri}> .
-//     }
-//     `);
-
-
-
-//   const uncertaintyFilter = Array.from(state.selectedUncertaintyKeywords || []).map(selectedLevel => `
-//       FILTER NOT EXISTS {
-//         ?factoid spq:certainty ?level .
-//         FILTER(LCASE(STR(?level)) = "${selectedLevel.toLowerCase()}")
-//       }
-//     `
-//     ).join('\n');
-
-//   const placePattern = state.place
-//     ? `
-//       {
-//         ?person swdt:birth-place <${state.place}> ;
-//                 sp:birth-place ?placeStatement .
-//       } UNION {
-//         ?person swdt:death-place <${state.place}> ;
-//                 sp:death-place ?placeStatement .
-//       } UNION {
-//         ?person swdt:residence <${state.place}> ;
-//                 sp:residence ?placeStatement .
-//       } UNION {
-//         ?event swdt:event-place <${state.place}> ;
-//                sp:event-place ?placeStatement .
-//       }
-//       ?placeStatement spr:reference-URL ?factoid .
-//     `
-//     : '';
-    
-
-//   const fieldOfStudyPattern = state.fieldOfStudy
-//     ? `
-//       ?statementNode sps:education <${state.fieldOfStudy}> ;
-//                      spr:reference-URL ?factoid .
-//     `
-//     : '';
-
-//   const unionBlocks = [...eventPatterns, ...relationshipPatterns];
-//   const unionBlock = unionBlocks.length
-//     ? `FILTER EXISTS {
-//         ${unionBlocks.join('\n        UNION\n')}
-//     }`
-//     : '';
-
-//   const additionalPatterns = [placePattern, fieldOfStudyPattern].filter(Boolean).join('\n');
-
-//   return `
-// PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-// PREFIX schema: <http://schema.org/>
-// PREFIX spr: <http://syriaca.org/prop/reference/>
-// PREFIX swdt: <http://syriaca.org/prop/direct/>
-// PREFIX sp: <http://syriaca.org/prop/>
-// PREFIX sps: <http://syriaca.org/prop/statement/>
-// PREFIX spq: <http://syriaca.org/prop/qualifier/>
-
-// SELECT DISTINCT ?factoid ?description ?person ?label ?gender ?relationship ?place ?ethnicity ?
-// FROM <http://syriaca.org/persons#graph>
-// FROM <https://spear-prosop.org>
-// WHERE {
-//   ?statementNode spr:reference-URL ?factoid .
-
-//   OPTIONAL { ?factoid schema:description ?description . }
-
-//   OPTIONAL {
-//     ?statementNode ?p ?person .
-//     ?person rdfs:label ?label .
-//     FILTER(LANG(?label) = "en")
-//   }
-
-//   ${ethnicityBlock}
-//   ${genderPattern}
-//   ${uncertaintyFilter}
-//   ${unionBlock}
-//   ${additionalPatterns}
-// }
-// ORDER BY ?label
-// LIMIT 200
-// `;
-// }
 export function buildFilterQuery(state) {
   const {
     selectedEventKeywords = [],
@@ -272,10 +164,17 @@ export function buildFilterQuery(state) {
   });
 
   // Ethnicity
-  selectedEthnicityKeywords.forEach(uri => {
-    tripleBlocks.push(`?person swdt:ethnic-label <${uri}> .`);
-    tripleBlocks.push(`?person sp:ethnic-label ?stmt .`);
-  });
+  if (selectedEthnicityKeywords.length > 0) {
+    const ethnicityUnion = selectedEthnicityKeywords.map(uri => `
+      {
+        ?person swdt:ethnic-label <${uri}> .
+        ?person sp:ethnic-label ?stmt .
+      }
+    `).join(' UNION ');
+
+    tripleBlocks.push(ethnicityUnion);
+  }
+
 
   // Gender: How is this handled in Neptune, need named graph block?
   selectedGenderKeywords.forEach(uri => {
@@ -312,7 +211,7 @@ PREFIX sp: <http://syriaca.org/prop/>
 PREFIX sps: <http://syriaca.org/prop/statement/>
 PREFIX spq: <http://syriaca.org/prop/qualifier/>
 
-SELECT DISTINCT ?factoid ?description
+SELECT DISTINCT ?factoid ?description ?label ?person ?type ?relationship ?event ?level ?stmt
 FROM <https://spear-prosop.org>
 WHERE {
   ${tripleBlocks.join('\n')}
@@ -533,7 +432,12 @@ export async function fetchFactoidsByMultiType(state) {
       uri: b.factoid?.value ?? '',
       description: b.description?.value ?? '',
       label: b.label?.value ?? '',
-      person: b.person?.value ?? ''
+      person: b.person?.value ?? '',
+      gender: b.gender?.value ?? '',
+      uncertainty: b.level?.value ?? '',
+      type: b.type?.value ?? '',
+      relationship: b.relationship?.value ?? '',
+      event: b.event?.value ?? ''
     }));
   } catch (err) {
     console.error("Failed to fetch factoids:", err);
